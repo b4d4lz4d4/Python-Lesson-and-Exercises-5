a3b25c71b780bac21929e297c6dd449ecccc48cf
R4m1q <50233975+r4m1q@users.noreply.github.com>
2022-08-16 06:11:41 +0400

    Python Lesson and Exercises-5
    Full Python

---
+++ rssn5.py
@@ -0,0 +1,22 @@
+# Cavab:1
+# list = [3, 12, 48, 7, 57, 41, 76, 62, 16, 48, 59, 53, 60, 79, 71, 81, 88, 26, 45, 72, 25, 65, 53, 88, 79, 34, 42, 40, 45, 92, 52, 85, 90, 16, 37, 50, 96, 67, 7, 59, 3, 41, 40, 58, 83, 16, 47, 23, 5, 22]
+
+# new_list = print([i**2 for i in list if i%6==0])
+#= = = = = = = = = = = = = = = = = = = = = = = =
+
+#Cavab:2
+# number = range(-100,100)
+# print([i*3 for i in number if i%7==0])
+#= = = = = = = = = = = = = = = = = = = = = = = =
+
+#Cavab:3
+# data = (1, 2, 3, True, False, 3.3) bu datanı buna çevirin ('asdf', 1, 2, 3, True, False, 3.3, (1, 2))
+# data = (1, 2, 3, True, False, 3.3)
+
+my_tuple = "asdf", 1, 2, 3, True, False, 3.3, (1, 2)
+
+print(my_tuple)
+#= = = = = = = = = = = = = = = = = = = = = = = =
+
+#Cavab:4
+
